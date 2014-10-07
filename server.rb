e32f9de962b9ddd193cff406ee5da3c33de45965
jamesascarter <jamesascarter@gmail.com>
2014-10-07 11:46:23 +0100

    created user class with login and password

--- server.rb
+++ server.rb
@@ -5,7 +5,10 @@ env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link' # this needs to be done after datamapper is initialised
+require './lib/link'
+require './lib/user'
+require './lib/tag'
+ # this needs to be done after datamapper is initialised
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -13,6 +16,8 @@ DataMapper.finalize
 # However, the database tables don't exist yet. Let's tell datamapper to create them
 DataMapper.auto_upgrade!
 
+	enable :sessions
+	set :session_secret, 'super secret'
 
   get '/' do
     @links = Link.all
@@ -34,4 +39,23 @@ DataMapper.auto_upgrade!
   	tag = Tag.first(:text => params[:text])
   	@links = tag ? tag.links : []
   	erb :index
+  end
+
+  get '/users/new' do
+  	erb :"users/new"
+  end	
+
+  post '/users' do
+  	user = User.create(:email => params[:email],
+  				:password => params[:password])
+  	session[:user_id] = user.id
+  	redirect to('/')
+  end	
+
+  helpers do
+
+  def current_user
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
+
   end
